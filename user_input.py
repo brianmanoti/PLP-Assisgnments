c8e8fc12c6e12a63081dc34337321a8f0051c776
Brian manoti <108587053+brianmanoti@users.noreply.github.com>
2024-02-20 06:48:20 +0300

    Simple Usage Of Input() function to take in user inputs

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Whats Your name? \n")
+age = input("whats Your Age? \n")
+location = input("Whats Your location ? \n")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
